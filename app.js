4cfcb4b36f747da1bcc0c6b067c5877786c7476c
zmewa079 <zannatul.mewa07@gmail.com>
2021-10-11 17:48:17 -0400

    completed guess the number lab

--- app.js
+++ app.js
@@ -19,5 +19,19 @@ const game = {
       this.render(playerInput)
     }
   },
+  render: function (playerInput)   { 
+    if(playerInput >= 0 && playerInput <= 100){
+      if (playerInput === this.secretNum) {
+        alert(`Congrats! You guessed the number in ${this.prevGuesses.length} guesses!`)
+      } 
+      else if (playerInput > this.secretNum){
+        alert(`Your guess is too high Previous guesses: ${this.prevGuesses.join(`, `)}`)
+      } 
+      else if (playerInput < this.secretNum){
+        alert(`Your guess is too low Previous guesses: ${this.prevGuesses.join(`, `)}`)
+      }
+    }
+  }
   }
   
+  game.play()
