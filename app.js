82b98344ebcae101c8f7a1e8e065bf62c3451c11
zmewa079 <zannatul.mewa07@gmail.com>
2021-10-11 17:40:18 -0400

    add parseInt and invoke getGuess in play method

--- app.js
+++ app.js
@@ -4,8 +4,20 @@ const game = {
   smallestNum: 1,
   secretNum: null,
   prevGuesses: [],
+  
+  getGuess: function() {
+    let playerGuess = prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}`) 
+    let parseInput = parseInt(playerGuess)
+    return parseInput
+  },
   play: function() {
-    this.secretNum = Math.floor(Math.random() * 
-      (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+    this.secretNum = Math.floor(Math.random() * (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+    let playerInput
+    while(playerInput !== this.secretNum){
+      playerInput = this.getGuess()
+      this.prevGuesses.push(playerInput)  
+      this.render(playerInput)
+    }
+  },
   }
-}
+  
